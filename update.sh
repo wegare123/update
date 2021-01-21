df0bbff72629f55f44e3f54bb26d860bfb26cf7f
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-01-21 15:03:10 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -12,4 +12,8 @@ wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/update/
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/update/main/cek.sh" -O /usr/bin/update-tools
 chmod +x /etc/banner2
 chmod +x /usr/bin/update-tools
+echo "fix stl method http"
+echo "fix sst no plugin"
+echo "donasi ovo: 083822389648"
+echo "donasi dana: 082318259593"
 echo "update selesai"
