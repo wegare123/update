8cb6324fa41cea1525aa26232c5283516e51c944
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-01-21 13:28:24 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#update (Wegare)
+opkg update && opkg install wget curl
+# stl
+wget -O /usr/bin/stl "https://www.dropbox.com/s/lfbaxm8ud37a6kr/stl.sh"
+chmod +x /usr/bin/stl
+# sst
+wget -O /usr/bin/sst "https://www.dropbox.com/s/qsml8r53ntcajqa/sst.sh"
+chmod +x /usr/bin/sst
+# banner
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/update/main/banner2" -O /etc/banner2
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/update/main/cek.sh" -O /usr/bin/update-tools
+chmod +x /etc/banner2
+chmod +x /usr/bin/update-tools
+echo "fix stl method http"
+echo "fix sst no plugin"
+echo "donasi ovo : 083822389648"
+echo "donasi dana : 082318259593"
+echo "update selesai"
