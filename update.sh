edd7d07df3e2474215846e66d118cf9d9e97d1ce
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-01-21 14:42:10 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #update (Wegare)
 opkg update
-wget --no-check-certificate "https://www.dropbox.com/s/lfbaxm8ud37a6kr/stl.sh" -O /usr/bin/stl 
-chmod +x /usr/bin/stl
-wget --no-check-certificate "https://www.dropbox.com/s/qsml8r53ntcajqa/sst.sh" -O /usr/bin/sst
-chmod +x /usr/bin/sst
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/update/main/banner2" -O /etc/banner2
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/update/main/cek.sh" -O /usr/bin/update-tools
 chmod +x /etc/banner2
