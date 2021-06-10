eec884bd181178ab57c5ecba43c576b8de7047b8
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-06-10 23:26:42 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -56,6 +56,8 @@ fi
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/stl.sh" -O /usr/bin/stl
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/gproxy.sh" -O /usr/bin/gproxy
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/autorekonek-stl.sh" -O /usr/bin/autorekonek-stl
+wget --no-check-certificate "https://github.com/wegare123/stl/blob/main/stl/Brainfuck-master.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && mv ~/Brainfuck-master/* ~/ && mv ~/Brainfuck-master/.brainfuck-tunnel ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/Brainfuck-master
+chmod +x /root/brainfuck
 chmod +x /usr/bin/gproxy
 chmod +x /usr/bin/stl
 chmod +x /usr/bin/autorekonek-stl
