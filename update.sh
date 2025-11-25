76f8b7670d46b74d81e083fcbec70c512b134e3e
Wega Regianto <56117745+wegare123@users.noreply.github.com>
2025-11-25 17:58:36 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -76,14 +76,14 @@ wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/tun2s
 chmod +x /usr/bin/tun2socks
 # stl
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/stl.sh" -O /usr/bin/stl
-wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/gproxy.sh" -O /usr/bin/gproxy
+#wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/gproxy.sh" -O /usr/bin/gproxy
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/autorekonek-stl.sh" -O /usr/bin/autorekonek-stl
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/tunnel.py" -O /root/akun/tunnel.py
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/ssh.py" -O /root/akun/ssh.py
 wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/stl/main/stl/inject.py" -O /root/akun/inject.py
 #wget --no-check-certificate "https://github.com/wegare123/stl/blob/main/stl/Brainfuck-master.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && mv ~/Brainfuck-master/* ~/ && mv ~/Brainfuck-master/.brainfuck-tunnel ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/Brainfuck-master
 #chmod +x /root/brainfuck
-chmod +x /usr/bin/gproxy
+#chmod +x /usr/bin/gproxy
 chmod +x /usr/bin/stl
 chmod +x /root/akun/tunnel.py
 chmod +x /root/akun/ssh.py
